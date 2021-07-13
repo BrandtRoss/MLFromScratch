328c7191384d3798b103cc27e27f0de740ddd9c0
B Ross <bross@pop-os.localdomain>
2021-07-12 22:59:11 -0600

    delta values for backprop are calculated

--- main.py
+++ main.py
@@ -2,33 +2,60 @@ import matplotlib.image as img
 import numpy as np
 
 # weight matricies
-w1 = np.ones((16,784))
-w2 = np.ones((16,16))
-w3 = np.ones((10,16))
+w1 = np.ones((16,784)) # (neurons in first hidden layer, neurons in input layer)
+w2 = np.ones((16,16))  # (neurons in second hidden layer, neurons in first hidden layer)
+w3 = np.ones((10,16))  # (neurons in output, neurons in second hidden layer)
 
 # bias matricies
-b1 = np.ones((16,1))
-b2 = np.ones((16,1))
-b3 = np.ones((10,1))
+b1 = np.ones((16,1)) # neurons in first hidden layer
+b2 = np.ones((16,1)) # neurons in second hidden layer
+b3 = np.ones((10,1)) # neurons in output layer
 
 def readData(imPath = 'dataset/test/0/1001.png'):
     # im is a 1d array that is the image from left to right then top to bottom
     image = img.imread(imPath)
     return np.array(image).flatten().reshape(784,1)
 
+# returns sigmoid of all values in array
 def sigmoid(arr):
     return 1/(1 + np.exp(-arr))
 
+# input is array with sigmoid applied
+# returns derivative of sigmoid for all values in array
+def sigmoidPrime(arr):
+    return arr * (np.ones((arr.size,1)) - arr)
+
+# calculates the neural net
+# returns a tuple of the activations for each layer
 def calcFromNet(image):
-    a1 = sigmoid(np.matmul(w1, image) + b1)
-    a2 = sigmoid(np.matmul(w2, a1) + b2)
-    a3 = sigmoid(np.matmul(w3, a2) + b3)
-    return a3
+    z1 = np.matmul(w1, image) + b1
+    a1 = sigmoid(z1)
+    z2 = np.matmul(w2, a1) + b2
+    a2 = sigmoid(z2)
+    z3 = np.matmul(w3, a2) + b3
+    a3 = sigmoid(z3)
+    return (a1, a2, a3)
+
+# input is the return from calcFromNet
+# adjusts all weight and bias values
+def backprop(results, expected):
+    del3 = (results[2] - expected) * sigmoidPrime(results[2])
+    del2 = np.matmul(w3.transpose(), del3) * sigmoidPrime(results[1])
+    del1 = np.matmul(w2.transpose(), del2) * sigmoidPrime(results[0])
+    # adjust bias
+    return (b1 + del1, b2 + del2, b3 + del3)
+    # adjust weight
 
 im = readData()
-#print(im)
-print(calcFromNet(im))
+res = calcFromNet(im)
+print(res[2])
+expect = np.zeros((10,1))
+expect[0] = 1
+b1, b2, b3 = backprop(res, expect)
+print(b3)
+res = calcFromNet(im)
+print(res[2])
 
 x = np.array([1,2,3])
-x = x.reshape(3,1)
 y = np.array([1,2,3])
+a = np.array([[1,2,3], [4,5,6]])
