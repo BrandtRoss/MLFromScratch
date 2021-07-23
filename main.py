118bdf76d143ec79e7ec136923a13ad7dcbe352e
bross <bross@mymail.mines.edu>
2021-07-23 02:49:38 -0600

    values start random and huge speedup in weight backprop

--- main.py
+++ main.py
@@ -3,16 +3,6 @@ import numpy as np
 import os
 import random
 
-# weight matricies
-w1 = np.ones((16,784)) # (neurons in first hidden layer, neurons in input layer)
-w2 = np.ones((16,16))  # (neurons in second hidden layer, neurons in first hidden layer)
-w3 = np.ones((10,16))  # (neurons in output, neurons in second hidden layer)
-
-# bias matricies
-b1 = np.ones((16,1)) # neurons in first hidden layer
-b2 = np.ones((16,1)) # neurons in second hidden layer
-b3 = np.ones((10,1)) # neurons in output layer
-
 # input is the root folder to traverse
 # returns an array of tuples of (
 def traverseFolder(rootFolder):
@@ -26,27 +16,40 @@ def traverseFolder(rootFolder):
 # input is training data from traverseFolder
 # returns tuple of (b1, b2, b3, w1, w2, w3)
 def trainNetwork(trains):
+    # define weight and bias matricies
+    # weight matricies
+    w1 = np.random.rand(16,784) # (neurons in first hidden layer, neurons in input layer)
+    w2 = np.random.rand(16,16)  # (neurons in second hidden layer, neurons in first hidden layer)
+    w3 = np.random.rand(10,16)  # (neurons in output, neurons in second hidden layer)
+
+    # bias matricies
+    b1 = np.random.rand(16,1) # neurons in first hidden layer
+    b2 = np.random.rand(16,1) # neurons in second hidden layer
+    b3 = np.random.rand(10,1) # neurons in output layer
+
     curr = 0
     length = len(trains)
     for i in trains:
         print('Training: %' + str(100*curr/length))
         im = readData(i[1])
-        res = calcFromNet(im)
-        expect = np.zeros((10,1))
-        expect[int(i[0])] = 1
-        b1, b2, b3, w1, w2, w3 = backprop(res, expect, im)
+        for j in range(30):
+            res = calcFromNet(im, (b1, b2, b3, w1, w2, w3))
+            expect = np.zeros((10,1))
+            expect[int(i[0])] = 1
+            b1, b2, b3, w1, w2, w3 = backprop(res, expect, im, (b1, b2, b3, w1, w2, w3))
+            del res
         curr += 1
-        del res
+        #break
     return (b1, b2, b3, w1, w2, w3)
 
-def testNetwork(tests):
+def testNetwork(tests, matricies):
     curr = 0
     length = len(tests)
     errors = [0,0,0,0,0,0,0,0,0,0]
     guesses = [0,0,0,0,0,0,0,0,0,0]
     for i in tests:
         im = readData(i[1])
-        res = calcFromNet(im)
+        res = calcFromNet(im, (b1, b2, b3, w1, w2, w3))
         guess = np.argmax(res[2])
         print(guess, res[2])
         guesses[guess] += 1
@@ -71,7 +74,8 @@ def sigmoidPrime(arr):
 
 # calculates the neural net
 # returns a tuple of the activations for each layer
-def calcFromNet(image):
+def calcFromNet(image, matricies):
+    b1, b2, b3, w1, w2, w3 = matricies
     z1 = np.matmul(w1, image) + b1
     a1 = sigmoid(z1)
     z2 = np.matmul(w2, a1) + b2
@@ -82,40 +86,32 @@ def calcFromNet(image):
 
 # input is the return from calcFromNet, the expected output, and the original input
 # adjusts all weight and bias values
-def backprop(results, expected, inp):
+def backprop(results, expected, inp, matricies):
+    b1, b2, b3, w1, w2, w3 = matricies
     del3 = (results[2] - expected) * sigmoidPrime(results[2])
     del2 = np.matmul(w3.transpose(), del3) * sigmoidPrime(results[1])
     del1 = np.matmul(w2.transpose(), del2) * sigmoidPrime(results[0])
-    # TODO: reduce loops done in python
     # adjust weights
     # adjust w3
-    newW3 = np.ones((10,16))
-    for j in range(10): # output layer
-        for k in range(16): # second hidden layer
-            newW3[j][k] = w3[j][k] + results[1][k] * del3[j]
+    newW3 = w3 - np.transpose(np.repeat(results[1], 10, axis=1)) * np.repeat(del3, 16, axis=1)
     # adjust w2
-    newW2 = np.ones((16,16))
-    for j in range(16): # 
-        for k in range(16): # 
-            newW2[j][k] = w2[j][k] + results[0][k] * del2[j]
+    newW2 = w2 - np.transpose(np.repeat(results[0], 16, axis=1)) * np.repeat(del2, 16, axis=1)
     # adjust w1
-    newW1 = np.ones((16,784))
-    for j in range(16): # 
-        for k in range(784): # 
-            newW1[j][k] = w1[j][k] + inp[k] * del1[j]
+    newW1 = w1 - np.transpose(np.repeat(inp, 16, axis=1)) * np.repeat(del1, 784, axis=1)
     # adjust bias
-    return (b1 + del1, b2 + del2, b3 + del3, newW1, newW2, newW3)
+    # TODO: I think the del calculations aren't right
+    return (b1 - del1, b2 - del2, b3 - del3, newW1, newW2, newW3)
 
 testData = traverseFolder('dataset/test')
 trainData = traverseFolder('dataset/training')
 x = trainNetwork(trainData)
 b1, b2, b3, w1, w2, w3 = x
-testNetwork(testData)
-print(trainData[0], trainData[-1])
-print(testData[0], testData[-1])
+testNetwork(testData, x)
+'''
 print('b1',b1)
 print('b2',b2)
 print('b3',b3)
 print('w1',w1)
 print('w2',w2)
 print('w3',w3)
+'''
